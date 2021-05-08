aa5a56ee13f7caa102907bd427cc7e370f4e0e8e
Atilio Grafión <atiliollgn@gmail.com>
2021-05-08 17:33:21 -0300

    datos pelicula

--- src/app/components/pelicula/pelicula.component.ts
+++ src/app/components/pelicula/pelicula.component.ts
@@ -10,6 +10,7 @@ import { PeliculasService } from 'src/app/services/peliculas.service';
 export class PeliculaComponent implements OnInit {
   pelicula:Pelicula;
   peliculas:Array<Pelicula>;
+  aux:Array<string>;
   constructor(private PeliculasService:PeliculasService) { 
     this.peliculas=new Array<Pelicula>();
     this.buscar("tt0848228");
@@ -27,12 +28,18 @@ export class PeliculaComponent implements OnInit {
     this.PeliculasService.buscarPelicula(id).subscribe(
       (result)=>{
         this.pelicula=new Pelicula();
-        console.log(result);
+        //console.log(result);    
+        this.aux=new Array<string>();
         this.pelicula.titulo=result.title;
         this.pelicula.desc=result.plot;
         this.pelicula.img=result.poster;
         this.pelicula.largo=result.length;
         this.pelicula.trailer=result.trailer.link;
+        result.cast.forEach(element => {
+          this.aux.push(element.actor);
+        });
+        this.pelicula.actores=this.aux;
+        console.log(this.pelicula.actores);
         this.peliculas.push(this.pelicula);
       },
       (error)=>{alert("Error en la petición");}
--- src/app/models/pelicula.ts
+++ src/app/models/pelicula.ts
@@ -4,11 +4,13 @@ export class Pelicula {
     img:string;
     largo:string;
     trailer:string;
-    constructor(titulo?:string, desc?:string,img?:string, largo?:string, trailer?:string){
+    actores:Array<string>;
+    constructor(titulo?:string, desc?:string,img?:string, largo?:string, trailer?:string, actores?:Array<string>){
         this.titulo=titulo;
         this.desc=desc;
         this.img=img;
         this.largo=largo;
         this.trailer=trailer;
+        this.actores=actores;
     }
 }
